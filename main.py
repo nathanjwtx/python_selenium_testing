981060a2361d1766c26f4b1df2bf33315ffb94eb
Nathan <nathanjwtx@nathanjwtx.io>
2021-02-11 08:14:17 -0600

    updated file setting code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from selenium import webdriver
+from pathlib import Path
 
 
 class SearchText(TestCase):
@@ -7,8 +8,9 @@ class SearchText(TestCase):
         self.driver = webdriver.Chrome("C:\\Users\\nathan\\chrome_driver\\chromedriver.exe")
         self.driver.implicitly_wait(30)
         # self.driver.maximize_window()
-        test_file = 'C:\\Users\\nathan\\repos\\selenium_testing\\testing.html'
-        self.driver.get(test_file)
+        # test_file = 'C:\\Users\\nathan\\repos\\selenium_testing\\testing.html'
+        test_file = Path.cwd() / 'testing.html'
+        self.driver.get(test_file.as_uri())
 
     def test_search_by_id(self):
         self.list_item = self.driver.find_element_by_id('first')
